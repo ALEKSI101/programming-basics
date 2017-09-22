11620c9fbbc983f79e2d0e2c0a9e4b867d18cb75
ALEKSI101 <Aleksi198_@hotmail.com>
2017-09-22 14:44:01 +0300

    task-1-2-and-3-complete

---
+++ conditional-statements/conditional-statement/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //define instructions
+            Console.WriteLine("Ohjelma tulostaa parillinen tai pariton");
+            bool isNumber;
+            Console.Write("Syötä luku: ");
+            string userInput;
+            userInput = Console.ReadLine();
+
+            //define variables
+
+            int evaluatedNumber;
+            isNumber = int.TryParse(userInput, out evaluatedNumber);
+
+            //program logic
+
+            if (evaluatedNumber % 2 == 0)
+            {
+                Console.WriteLine($"Syötit luvun {evaluatedNumber}, se on parillinen");
+            }
+            else
+            {
+                Console.WriteLine($"Syötit luvun {evaluatedNumber}, se on pariton");
+            }
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ conditional-statements/conditional-statement/task-2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa parillinen tai pariton");
+            bool isNumber;
+            Console.Write("Syötä luku: ");
+            string userInput;
+            userInput = Console.ReadLine();
+
+            int evaluatedNumber;
+            isNumber = int.TryParse(userInput, out evaluatedNumber);
+
+            if (evaluatedNumber % 2 == 0)
+            {
+                Console.WriteLine($"Syötit luvun {evaluatedNumber}, se on parillinen");
+            }
+            else
+            {
+                Console.WriteLine($"Syötit luvun {evaluatedNumber}, se on pariton");
+            }
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
