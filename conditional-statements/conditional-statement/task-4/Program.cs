fe00b9f2d3d92981e9fda554eebe835b3cb4c0d5
ALEKSI101 <Aleksi198_@hotmail.com>
2017-09-29 11:12:58 +0300

    final versio of 1.4A

---
+++ conditional-statements/conditional-statement/task-4/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma lajittelee luvut nousevaan järjestykseen.");
+
+            Console.WriteLine("Syötä ensimmäinen luku: ");
+            string numero1 = Console.ReadLine();
+            int numx = int.Parse(numero1);
+
+            Console.WriteLine("Syötä toinen luku: ");
+            string numero2 = Console.ReadLine();
+            int numy = int.Parse(numero2);
+
+            Console.WriteLine("Syötä kolmas luku: ");
+            string numero3 = Console.ReadLine();
+            int numz = int.Parse(numero3);
+
+
+
+            if (numx < numy)
+            {
+                if (numx < numz)
+                {
+
+                    if (numy < numz)
+                    {
+                        Console.WriteLine($"{numx}, {numy}, {numz}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{numx},{numz },{numy }");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine($"{numz},{numx},{numy}");
+                }
+
+            }
+            else
+            {
+
+                if (numx < numz)
+                {
+
+                    Console.WriteLine($"{numy},{numx},{numz}");
+                }
+                else
+                {                 
+                 
+                    if (numy < numz)
+                    {
+                        Console.WriteLine($"{numy},{numz},{numx}");
+                    }
+
+                    else
+                    {
+                        Console.WriteLine($"{numz},{numy},{numx}");
+                    }
+                        
+                }
+                
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
+            
+
+       
+
+
+
+
+
+
+
+
+
+         
