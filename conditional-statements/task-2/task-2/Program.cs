1503fc93ec1b8d659eeaf97b261e3ea60d2bc07f
ALEKSI101 <Aleksi198_@hotmail.com>
2017-10-05 10:21:31 +0300

    task 2 done

---
+++ conditional-statements/task-2/task-2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma laskee kertoman. Anna luku, jonka kertoma lasketaan. ");
+            
+            string userInput = Console.ReadLine();
+            int number = int.Parse(userInput);
+            //int.TryParse(userInput, out int number)
+            int i = 0;
+            int f = 1;
+            do
+            {
+                i = i + 1;
+                f = f * i;
+                //Console.WriteLine($"{i}!={f}"); // 5!=120
+            } while (i < number);
+
+            Console.WriteLine($"Syötit: { number}\n Vastaus: { f}"); // 5!=120
+            Console.ReadKey();
+            
+                 
+        }
+        
+    }
+}
