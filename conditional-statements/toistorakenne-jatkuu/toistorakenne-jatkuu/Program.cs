11f84e281604ae5af3a2648b0e0ba81ad6b57778
ALEKSI101 <Aleksi198_@hotmail.com>
2017-10-23 14:34:48 +0300

    finalized versions until task 4

---
+++ conditional-statements/toistorakenne-jatkuu/toistorakenne-jatkuu/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toistorakenne_jatkuu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma tulostaa kokonaisluvut 1-10 ja niiden neliöjuuret ");
+
+            int i = 1;
+            while (i <= 10)
+            {
+                Console.WriteLine($"{i}\t\t{Math.Round(Math.Sqrt(i), 2)}");
+                i++;
+            }
+            Console.ReadKey();
+
+        }
+
+    }
+}
