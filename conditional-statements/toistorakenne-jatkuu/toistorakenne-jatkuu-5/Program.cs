9b2d0f650032bea87f088a3b888334103c356e00
ALEKSI101 <Aleksi198_@hotmail.com>
2017-11-02 13:41:09 +0200

    almost ready

---
+++ conditional-statements/toistorakenne-jatkuu/toistorakenne-jatkuu-5/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace toistorakenne_jatkuu_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+
+            Console.WriteLine("Ohjelma tulostaa vakioveikkauksen rivin. ");
+            
+
+            double numx;
+            numx = rnd.NextDouble();
+
+            if (numx <= 0.4)
+            {
+                Console.WriteLine(" 1 ");
+            }
+            else if (numx < 0.6)
+            {
+                Console.WriteLine(" X ");
+            }
+            else Console.WriteLine(" 2 ");
+
+            for (= 13;  ++)
+               
+            
+            Console.ReadKey();
+
+        }  
+                               
+        
+    }
+}
